3dee0a03ccce178c6771d174bb64315ccd0c17a3
kaizoku-619 <garssallaoui.bayrem@gmail.com>
2019-09-15 19:28:54 -0500

    fixing indentation

--- src/main.c
+++ src/main.c
@@ -18,7 +18,7 @@ int main(void)
               "Task 1"              ,	/* Text name for the task.  This is to facilitate debugging only. */
               1000                  ,	/* Stack depth - most small microcontrollers will use much less stack than this. */
               NULL                  ,	/* We are not using the task parameter. */
-              1 ,	                    /* This task will run at priority 1. */
+              1                     ,	/* This task will run at priority 1. */
               NULL);		              /* We are not using the task handle. */
 
   /* Create the other task in exactly the same way. */
