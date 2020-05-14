37da12f6aa278390ad8b903834be36d85d899782
kaizoku-619 <garssallaoui.bayrem@gmail.com>
2020-05-14 18:24:36 +0100

    add doc

--- include/main.h
+++ include/main.h
@@ -1,9 +1,56 @@
+/* 
+ **************************************************************************************************
+ *
+ * @file    : main.h
+ * @author  : Bayrem GHARSELLAOUI
+ * @version : 1.4.0
+ * @date    : May 2020
+ * @brief   : Application entry point header file
+ * 
+ **************************************************************************************************
+ * 
+ * @project   : {pio-freertos}
+ * @board     : {Nucleo 144 STM32F767Zi}
+ * @compiler  : {PlatformIO, version 4.3.3}
+ * @framework : {stm32cube}
+ * 
+ **************************************************************************************************
+ */
+
+/**
+  @mainpage Project overview
+  @brief Hi! Welcome to pio-freertos project documentation.<br>
+  This project is designed to be an rtos implementation on PlatformIO.<br>
+  The Git project path is https://github.com/kaizoku-619/pio-freertos.git<br>
+  For further details, please refer to documentation available in doc/ directory.<br>
+*/
+
+/** @defgroup main MAIN
+  * @brief Project entry point.
+  * @{
+  */
+
 #ifndef MAIN_H
 #define MAIN_H
 
+/*-----------------------------------------------------------------------------------------------*/
+/* Includes                                                                                      */
+/*-----------------------------------------------------------------------------------------------*/
+/** @defgroup main_includes Includes
+ * @{
+ */
 #include "stm32f7xx_nucleo_144.h"
 #include "cmsis_os.h"
+/**
+  * @}
+  */
 
+/*-----------------------------------------------------------------------------------------------*/
+/* Defines                                                                                       */
+/*-----------------------------------------------------------------------------------------------*/
+/** @defgroup main_defines Defines
+ * @{
+ */
 /* Pins Config */
 #define USARTx_TX_PIN                            GPIO_PIN_8
 #define USARTx_RX_PIN                            GPIO_PIN_9
@@ -16,7 +63,16 @@
 
 /* USART instance */
 #define USARTx                                   USART3
+/**
+  * @}
+  */
 
+/*-----------------------------------------------------------------------------------------------*/
+/* Macros                                                                                        */
+/*-----------------------------------------------------------------------------------------------*/
+/** @defgroup main_alice_macros Macros
+ * @{
+ */
 /* Clocks Config */
 #define USARTx_CLK_ENABLE()                      __HAL_RCC_USART3_CLK_ENABLE()
 #define USARTx_CLK_DISABLE()                     __HAL_RCC_USART3_CLK_DISABLE()
@@ -27,5 +83,12 @@
 /* USART Resets */
 #define USARTx_FORCE_RESET()                     __HAL_RCC_USART3_FORCE_RESET()
 #define USARTx_RELEASE_RESET()                   __HAL_RCC_USART3_RELEASE_RESET()
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
 
 #endif /* MAIN_H */
--- include/stm32f7xx_it.h
+++ include/stm32f7xx_it.h
@@ -1,6 +1,36 @@
+/* 
+ **************************************************************************************************
+ *
+ * @file    : stm32f7xx_it.h
+ * @author  : Bayrem GHARSELLAOUI
+ * @version : 1.4.0
+ * @date    : May 2020
+ * @brief   : Interrupt Service Routines header file
+ * 
+ **************************************************************************************************
+ * 
+ * @project   : {pio-freertos}
+ * @board     : {Nucleo 144 STM32F767Zi}
+ * @compiler  : {PlatformIO, version 4.3.3}
+ * @framework : {stm32cube}
+ * 
+ **************************************************************************************************
+ */
+
+/** @defgroup stm32f7xx_it STM32F7XX_IT
+  * @brief Interrupt Service Routines.
+  * @{
+  */
+
 #ifndef __STM32F7xx_IT_H
 #define __STM32F7xx_IT_H
 
+/*-----------------------------------------------------------------------------------------------*/
+/* Exported function prototypes -----------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------------------------*/
+/** @defgroup stm32f7xx_it_exported_function_prototypes Exported function prototypes
+  * @{
+  */
 void EXTI15_10_IRQHandler(void);
 void USART3_IRQHandler(void);
 void NMI_Handler(void);
@@ -8,5 +38,8 @@ void HardFault_Handler(void);
 void MemManage_Handler(void);
 void BusFault_Handler(void);
 void UsageFault_Handler(void);
+/**
+  * @}
+  */
 
 #endif /* __STM32F7xx_IT_H */
